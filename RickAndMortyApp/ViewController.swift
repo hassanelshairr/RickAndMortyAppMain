8915dfabd5cf8e82a449e948ce63e6f8e956755d
hassan elshair <hassan.elshairr@gmail.com>
2025-08-26 14:49:32 +0300

    Initial Commit

---
+++ RickAndMortyApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RickAndMortyApp
+//
+//  Created by hassan elshaer on 26/08/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
